d966adcd6d65ef0aabfed74148cd574a3b4d278e
Heaven <djyesu@gmail.com>
2019-08-21 23:50:41 +0800

    修正错误,现在此dockerfile可以独立打包为镜像

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,44 @@
 #!/bin/sh
 
-#frps ip地址
-remote_ip=${remote_ip}
 
-#配置文件中用于描述服务的标记名称
-service_name=${service_name}
-#配置文件中用于描述frc所在位置的标记名称
-position_note=${position_note}
-##配置文件中本地服务的端口号
-local_port=${local_port}
-##配置文件中本地服务的协议
-service_type=${service_type}
+if $(env|grep server_addr > /dev/null );then
+#将容器启动时注入的变量保存到/etc/profile
+echo  "export $(env|grep server_addr)" >> /etc/profile
+fi
 
 
-sed -i "s#0.0.0.0#${remote_ip}#g" /usr/local/frp/frpc-lite.ini
+if [ -z "$server_addr" ]; then
+	server_addr=0.0.0.0
+fi
+if [ -z "$server_port" ]; then
+	server_port=7100
+fi
+if [ -z "$privilege_token" ]; then
+	privilege_token=405520
+fi
+if [ -z "$login_fail_exit" ]; then
+	login_fail_exit=true
+fi
 
-echo "
-[range:${service_name} ${position_note}]
-type = ${service_type}
-local_ip = 127.0.0.1
-local_port = ${local_port}
-remote_port = 0
-use_encryption = true
-use_compression = true
-" >> /usr/local/frp/frpc-lite.ini
+if [ -z "$hostname_in_docker" ]; then
+    hostname_in_docker=hostname_in_docker
+fi
 
-/usr/local/frp/frpc -c /usr/local/frp/frpc-lite.ini
+if [ -z "$ip_out_docker" ]; then
+    ip_out_docker=127.0.0.1
+fi
+
+if [ -z "$ssh_port_out_docker" ]; then
+    ssh_port_out_docker=22
+fi
+
+export config_file_frpc=/etc/frp/frpc-lite.ini
+sudo sed -i 's/server_addr = 0.0.0.0/server_addr = '$server_addr'/g' ${config_file_frpc}
+sudo sed -i 's/server_port = 7100/server_port = '$server_port'/g' ${config_file_frpc}
+sudo sed -i 's/privilege_token = 12345678/privilege_token = '$privilege_token'/g' ${config_file_frpc}
+sudo sed -i 's/login_fail_exit = true/login_fail_exit = '$login_fail_exit'/g' ${config_file_frpc}
+sudo sed -i 's/hostname_in_docker/'$hostname_in_docker'/g' ${config_file_frpc}
+sudo sed -i 's/ip_out_docker/'$ip_out_docker'/g' ${config_file_frpc}
+sudo sed -i 's/ssh_port_out_docker/'$ssh_port_out_docker'/g' ${config_file_frpc}
+
+/usr/bin/frpc -c ${config_file_frpc} 
